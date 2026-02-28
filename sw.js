d462727a4a028371034ae344fcb2ea907971f458
Elasxny <141901353+Elasxny@users.noreply.github.com>
2026-02-28 10:45:21 +0800

    Site updated: 2026-02-28 10:45:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770291926664";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772246703502";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
